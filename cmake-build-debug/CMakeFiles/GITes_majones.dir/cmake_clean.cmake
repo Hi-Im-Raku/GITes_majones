9268a53c126976ea0827ee7dca61ded85019d15f
Hi-Im-Raku <240822@edu.p.lodz.pl>
2022-01-13 19:01:50 +0100

    komicja wiadomosci nastepnej jeszcze i jeszcze

---
+++ cmake-build-debug/CMakeFiles/GITes_majones.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GITes_majones.dir/main.c.obj"
+  "GITes_majones.exe"
+  "GITes_majones.exe.manifest"
+  "GITes_majones.pdb"
+  "libGITes_majones.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GITes_majones.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
